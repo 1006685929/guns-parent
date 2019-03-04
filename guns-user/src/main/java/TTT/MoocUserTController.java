e33eb2796980c7689a3eb3a86bf2c3dc89db5eb7
1006685929 <1006685929@qq.com>
2019-03-04 16:40:22 +0800

    Initial commit

---
+++ guns-user/src/main/java/TTT/MoocUserTController.java
@@ -0,0 +1,21 @@
+package TTT;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ * 用户表 前端控制器
+ * </p>
+ *
+ * @author hxk
+ * @since 2019-03-02
+ */
+@Controller
+@RequestMapping("/moocUserT")
+public class MoocUserTController {
+
+}
+
